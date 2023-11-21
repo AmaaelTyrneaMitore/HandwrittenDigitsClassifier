5ff4c8c6a32f5fad16d869ed3a72711a83c46452
Shashank Singh <96770927+AmaaelTyrneaMitore@users.noreply.github.com>
2023-11-21 21:35:55 +0530

    🌟 Update: Optimized data loading and memory usage for enhanced efficiency

--- src/app.js
+++ src/app.js
@@ -3,45 +3,60 @@ import _ from 'lodash';
 
 import LogisticRegression from './models/logistic-regression.js';
 
-// Retrieve MNIST test & training data for digits 0 to 9
-console.log('\n\n[+] Retrieving MNIST Data...');
-const MNISTTrainingData = MNISTData.training(0, 60000);
-const MNISTTestData = MNISTData.testing(0, 1500);
+// Function to load and preprocess MNIST data
+const loadData = () => {
+  // Retrieve MNIST test & training data for digits 0 to 9
+  console.log('\n\n[+] Retrieving MNIST Data...');
+  const MNISTTrainingData = MNISTData.training(0, 60000);
+  const MNISTTestData = MNISTData.testing(0, 1500);
 
-// Flatten image data: convert 2D array of ima s to 1D array of pixel intensities
-console.log('[+] Flattening Image Data...');
-const flattenedFeatures = MNISTTrainingData.images.values.map((image) => _.flatMap(image));
-const flattenedTestFeatures = MNISTTestData.images.values.map((image) => _.flatMap(image));
+  // Flatten image data: convert 2D array of images to 1D array of pixel intensities
+  console.log('[+] Flattening Image Data...');
+  const flattenedFeatures = MNISTTrainingData.images.values.map((image) => _.flatMap(image));
+  const flattenedTestFeatures = MNISTTestData.images.values.map((image) => _.flatMap(image));
 
-// Encode labels: convert numeric labels (0-9) to one-hot encoded format
-console.log('[+] Encoding Labels...');
-const encodedLabels = MNISTTrainingData.labels.values.map((label) => {
-  // Create an array of zeros with length equal to the number of possible labels (10 for digits 0-9)
-  const encodedLabel = new Array(10).fill(0);
-  // Set the index representing the label to 1 (one-hot encoding)
-  encodedLabel[label] = 1;
-  return encodedLabel;
-});
+  // Encode labels: convert numeric labels (0-9) to one-hot encoded format
+  console.log('[+] Encoding Labels...');
+  const encodedLabels = MNISTTrainingData.labels.values.map((label) => {
+    const encodedLabel = new Array(10).fill(0); // Create an array for one-hot encoding
+    encodedLabel[label] = 1; // Set the index representing the label to 1 (one-hot encoding)
+    return encodedLabel;
+  });
 
-const encodedTestLabels = MNISTTestData.labels.values.map((label) => {
-  // Create an array of zeros with length equal to the number of possible labels (10 for digits 0-9)
-  const encodedLabel = new Array(10).fill(0);
-  // Set the index representing the label to 1 (one-hot encoding)
-  encodedLabel[label] = 1;
-  return encodedLabel;
-});
+  const encodedTestLabels = MNISTTestData.labels.values.map((label) => {
+    const encodedLabel = new Array(10).fill(0); // Create an array for one-hot encoding
+    encodedLabel[label] = 1; // Set the index representing the label to 1 (one-hot encoding)
+    return encodedLabel;
+  });
+
+  // Return the loaded and preprocessed data
+  return {
+    features: flattenedFeatures,
+    labels: encodedLabels,
+    testFeatures: flattenedTestFeatures,
+    testLabels: encodedTestLabels,
+  };
+};
+
+// Load and preprocess the MNIST data
+const { features, labels, testFeatures, testLabels } = loadData();
 
 console.log('[+] Initializing Logistic Regression...');
 
-const regression = new LogisticRegression(flattenedFeatures, encodedLabels, {
+// Initialize Logistic Regression model with preprocessed data
+const regression = new LogisticRegression(features, labels, {
   learningRate: 1,
   iterations: 50,
   batchSize: 300,
 });
 
 console.log('[+] Initiating Regression Training...');
+
+// Train the Logistic Regression model
 regression.train();
 
 console.log('[+] Calculating Model Accuracy...');
-const modelAccuracy = regression.test(flattenedTestFeatures, encodedTestLabels);
+
+// Test the trained model and calculate its accuracy
+const modelAccuracy = regression.test(testFeatures, testLabels);
 console.log(`[+] Model Accuracy: ${modelAccuracy * 100}%\n\n`);
